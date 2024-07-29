848a1eebd287c139a6812f252848ca11bda03a53
ashjostan7 <ashjostan7@gmail.com>
2024-07-29 11:43:54 +0530

    Casual attantion addition

--- attention.py
+++ attention.py
@@ -20,5 +20,29 @@ class SelfAttention_v1(nn.Module):
 
         return context_vec
     
-    import torch.nn as nn
 
+# Making the W_query, W_key and W_value a linear layer (with bias set to false) is an effecient way of doing matrix multiplication.
+# Parameter initilisation is better with the linear layer too. Better than rand function. 
+# linear layer without bias ->matrix multiplacation between parameter of that layer and argument passed to that layer. 
+
+import torch
+import torch.nn as nn
+class SelfAttention_v2(nn.Module):
+
+    def __init__(self, d_in, d_out, qkv_bias = False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias = qkv_bias)
+    
+    def forward(self, x):
+
+        keys = self.W_key(x)
+        query = self.W_query(x)
+        value = self.W_value(x)
+
+        attn_scores = query @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        context_vec = attn_weights @ value
+
+        return context_vec
