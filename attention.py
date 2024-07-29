e8ad6de23f6738c658ee5d9826df5727ad73cc96
ashjostan7 <ashjostan7@gmail.com>
2024-07-29 23:48:43 +0530

    Add causal attention and multi-head attention

--- attention.py
+++ attention.py
@@ -23,7 +23,7 @@ class SelfAttention_v1(nn.Module):
 
 # Making the W_query, W_key and W_value a linear layer (with bias set to false) is an effecient way of doing matrix multiplication.
 # Parameter initilisation is better with the linear layer too. Better than rand function. 
-# linear layer without bias ->matrix multiplacation between parameter of that layer and argument passed to that layer. 
+# linear layer without bias -> matrix multiplacation between parameter of that layer and argument passed to that layer. 
 
 import torch
 import torch.nn as nn
@@ -46,3 +46,89 @@ class SelfAttention_v2(nn.Module):
         context_vec = attn_weights @ value
 
         return context_vec
+    
+'''
+Causal Attention Class:
+- Adding Dropout.
+- Ability to handle batches of data.
+'''
+
+class CausalAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout=nn.Dropout(dropout)
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length),diagonal=1)
+        )
+        '''
+        REGISTER BUFFERS:
+        1) registering a tensor as a buffer can make our lives a lot easier: 
+           We don't have to remember to move tensors to a target device like a GPU manually.
+        2) Another advantage of PyTorch buffers, over regular tensors, 
+           is that they get included in a model's state_dict
+        '''
+    
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # b ---> is the batch dimension. 
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1,2)
+        attn_scores.masked_fill( self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(attn_scores/ keys.shape[-1] ** 0.05,dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+class MutliHeadAttentionWrapper(nn.Module):
+    '''
+    Wrapper class to create multiple heads of Causal attention class
+    '''
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList([CausalAttention(d_in,d_out,context_length, dropout , qkv_bias) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+
+
+# Using a wrapper makes the attention model work, but is not an effecient way to run it
+# The wrapper uses a for loop, therefore the attention is not calculated parallely across heads. 
+class MultiClassAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_lenght, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+
+        # d_out must be divisible by num_heads
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
+
+        self.d_out= d_out
+        self.num_heads= num_heads
+        self.head_dim == d_out // num_heads
+
+        self.W_query= nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key= nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value= nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout= nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_lenght, context_lenght), diagonal =1)
+        )
+
+    def forward(self,x):
+
+        b, num_tokens, d_in = x.shape
+
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        
+
+
